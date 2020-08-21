3660c61b8d1e786b2c8771a978390d50f1cdea99
skniro tceh team <1920389694@qq.com>
2020-08-21 16:47:44 +0800

    1.15.2-4.1.3.svt2 build13820 forge1.15.2-31.2.36

---
+++ 启动服务器.bat
@@ -0,0 +1,33 @@
+@ECHO OFF
+title Minecraft服务器
+SET BINDIR=%~dp0
+:_MENU
+CLS
+set tm1=%time:~0,2%
+set tm2=%time:~3,2%
+set tm3=%time:~6,2%
+@echo. 
+echo  现在时间：%date% %tm1%点%TM2%分
+@echo.
+@echo. 请按下任意键来启动服务器…………
+@echo.
+pause
+cls
+@echo.     
+@echo. 现在时间：%date% %tm1%点%TM2%分
+@echo. ----------------------------------------------------------------- 
+@echo. 
+@echo. 
+@echo.                   服务器正在启动中,请稍等……
+@echo. 
+@echo.
+@echo. -----------------------------------------------------------------
+java -Xms4096M -Xmx4096M -XX:+AggressiveOpts -XX:+UseCompressedOops -jar forge-1.15.2-31.2.36.jar
+@echo.
+@echo. ----------------------------------------------------------------- 
+@echo.               Minecraft --- 玩家数据保存完毕 已关服
+@echo.
+@echo.                         按任意键关闭该窗口
+@echo. -----------------------------------------------------------------
+pause
+EXIT
